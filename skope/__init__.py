56cae565e8c9d98d75d6959f3ad4fd72801caca3
Nicholas Saunders <nksaunders1994@gmail.com>
2017-10-27 11:08:03 -0400

    update

--- skope/__init__.py
+++ skope/__init__.py
@@ -1,1 +1,12 @@
-from .simulateK2target import Target
+# Version number
+__version__ = "0.0.1"
+
+# Was skope imported from setup.py?
+try:
+    __SKOPE_SETUP__
+except NameError:
+    __SKOPE_SETUP__ = False
+
+if not __SKOPE_SETUP__:
+
+    from .simulateK2target import Target
