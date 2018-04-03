dda9f54d280c05d2cdad1855ab283902d93e5c3d
Nicholas Saunders <nksaunders1994@gmail.com>
2018-04-02 19:41:03 -0700

    tpf pwd

--- skope/batch.py
+++ skope/batch.py
@@ -18,7 +18,7 @@ m_mags = np.arange(0., 22, 5)
 def Simulate(arg):
     iter, mag, m_mag = arg
     print("Running mag = %.2f, m_mag = %.2f" % (mag, m_mag))
-    sK2 = skope.Target(ftpf = os.path.expanduser('~/.kplr/data/k2/target_pixel_files/205998445/ktwo205998445-c03_lpd-targ.fits.gz'))
+    sK2 = skope.Target(ftpf = os.path.expanduser('/usr/lusers/nks1994/skope/.kplr/data/k2/target_pixel_files/205998445/ktwo205998445-c03_lpd-targ.fits.gz'))
     fpix, flux, ferr = sK2.GenerateLightCurve(mag, roll=m_mag, ncadences=1000)
     np.savez('batch/%2dmag%.2fmotion%.2f' % (iter, mag, m_mag), fpix=fpix, flux=flux)
 
