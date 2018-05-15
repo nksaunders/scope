f71e40c98e0b8832bf91cabff81cca04140ae2e0
Nicholas Saunders <nksaunders1994@gmail.com>
2018-05-15 11:50:06 -0700

    sensitivity tweak

--- skope/simulateK2target.py
+++ skope/simulateK2target.py
@@ -101,8 +101,8 @@ class Target(object):
         if not self.custom_ccd:
 
             # cx,cy: intra-pixel variation polynomial coefficients in x,y
-            self.cx = [1.0, 0.0, -0.3]
-            self.cy = [1.0, 0.0, -0.3]
+            self.cx = [1.0, 0.0, -0.2]
+            self.cy = [1.0, 0.0, -0.2]
 
             # x0,y0: center of PSF, half of aperture size plus random deviation
             x0 = (self.apsize / 2.0) + 0.2 * np.random.randn()
