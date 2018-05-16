cc2ea90b6c1ceec45cc7079eba8eb3a5b0de7359
Nicholas Saunders <nksaunders1994@gmail.com>
2018-05-15 18:31:58 -0700

    mcmc function

--- skope/psffit.py
+++ skope/psffit.py
@@ -11,9 +11,15 @@ import os
 from scipy.optimize import fmin_powell
 from .skopemath import PSF, PLD
 
+import scipy
+import scipy.misc
+scipy.derivative = scipy.misc.derivative
+import pymc
+from astroML.plotting.mcmc import plot_mcmc
+
 class PSFFit(object):
 
-    def __init__(self, fpix, ferr, xpos, ypos):
+    def __init__(self, fpix, ferr, xpos, ypos, ccd_args, apsize):
 
         # initialize self variables
         self.nsrc = 2
@@ -23,31 +29,24 @@ class PSFFit(object):
         self.ferr = ferr
         self.xpos = xpos
         self.ypos = ypos
+        self.ccd_args = ccd_args
+        self.apsize = apsize
 
     def Residuals(self, params):
         '''
 
         '''
 
-        amp1,amp2,x01,x02,y01,y02,sx1,sx2,sy1,sy2,rho1,rho2 = params
+        amp1,amp2,x01,x02,y01,y02,sx,sy,rho = params
         cadence = self.cadence
 
         # constrain parameter values
-        if sx1 > 1 or sx1 < 0:
+        if sx > 1 or sx < 0:
             return 1.0e30
-        if sy1 > 1 or sy1 < 0:
-            return 1.0e30
-
-        if rho1 >= 0.5 or rho1 <= -0.5:
+        if sy > 1 or sy < 0:
             return 1.0e30
 
-        # constrain parameter values
-        if sx2 > 1 or sx2 < 0:
-            return 1.0e30
-        if sy2 > 1 or sy2 < 0:
-            return 1.0e30
-
-        if rho2 >= 0.5 or rho2 <= -0.5:
+        if rho >= 0.5 or rho <= -0.5:
             return 1.0e30
 
 
@@ -102,11 +101,57 @@ class PSFFit(object):
 
         '''
 
-        amp1,amp2,x01,x02,y01,y02,sx1,sx2,sy1,sy2,rho1,rho2 = params
+        amp1,amp2,x01,x02,y01,y02,sx,sy,rho = params
 
         if neighbor:
             amp1 = 0
 
-        computed_psf = PSF(np.array([[amp1,amp2],[x01,x02],[y01,y02],[sx1,sx2],[sy1,sy2],[rho1,rho2]]), self.ccd_args, self.xpos[cadence], self.ypos[cadence])[0]
+        computed_psf = PSF(np.array([[amp1,amp2],[x01,x02],[y01,y02],[sx],[sy],[rho]]), self.ccd_args, self.xpos[cadence], self.ypos[cadence])[0]
 
         return computed_psf
+
+
+    def MCMC_results(self, cadence=0):
+
+        apsize = self.apsize
+
+        # Set up MCMC sampling
+        amp1 = pymc.Uniform('amp1', 200000., 300000., value=250000.)
+        amp2 = pymc.Uniform('amp2', 50000., 200000., value=100000.)
+        x01 = pymc.Uniform('x01', 0, apsize, value=(apsize / 2))
+        x02 = pymc.Uniform('x02', 0, apsize, value=(apsize / 2))
+        y01 = pymc.Uniform('y01', 0, apsize, value=(apsize / 2))
+        y02 = pymc.Uniform('y02', 0, apsize, value=(apsize / 2))
+        sx = pymc.Uniform('sx', 0, 1, value=0.5)
+        sy = pymc.Uniform('sy', 0, 1, value=0.5)
+        rho = pymc.Uniform('rho', -1, 1, value=0.01)
+
+        @pymc.deterministic
+        def psf_model(amp1=amp1, amp2=amp2, x01=x01, x02=x02, y01=y01, y02=y02, sx=sx, sy=sy, rho=rho):
+            return PSF(np.array([[amp1,amp2],[x01,x02],[y01,y02],[sx],[sy],[rho]]), self.ccd_args, self.xpos[cadence], self.ypos[cadence])[0]
+
+        y = pymc.Normal('y', mu=psf_model, tau=self.ferr[cadence], observed=True, value=self.fpix[cadence])
+
+        model = dict(amp1=amp1, amp2=amp2, x01=x01, x02=x02, y01=y01, y02=y02, sx=sx, sy=sy, rho=rho)
+
+
+        # Run the MCMC sampling
+        def compute_MCMC_results(niter=250, burn=40):
+            S = pymc.MCMC(model)
+            S.sample(iter=niter, burn=burn)
+            traces = [S.trace(s)[:] for s in ['amp1', 'amp2', 'x01', 'x02', 'y01', 'y02', 'sx', 'sy', 'rho']]
+
+            M = pymc.MAP(model)
+            M.fit()
+            fit_vals = (M.amp1.value, M.amp2.value, M.x01.value, M.x02.value, M.y01.value, M.y02.value, M.sx.value, M.sy.value, M.rho.value)
+
+            return traces, fit_vals
+
+        traces, fit_vals = compute_MCMC_results()
+
+        labels = ['amp1', 'amp2', 'x01', 'x02', 'y01', 'y02', 'sx', 'sy', 'rho']
+        fig = pl.figure(figsize=(10, 10))
+
+        plot_mcmc(traces, labels=labels, fig=fig, bins=30, colors='k')
+
+        return fit_vals
