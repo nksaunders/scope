87548c963059737989ab1416730d986f5b390caa
Nicholas Saunders <nksaunders1994@gmail.com>
2018-09-04 10:24:10 -0700

    ftpf path fix for travis

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -17,7 +17,7 @@ import scope
 def test_all():
 
     print('Testing scope...')
-    star = scope.Target()
+    star = scope.Target(ftpf='.kplr/data/k2/target_pixel_files/205998445')
     star.GenerateLightCurve(ncadences=1)
 
     print('Success!')
