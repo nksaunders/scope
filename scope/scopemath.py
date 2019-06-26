fix normalization

scope/scopemath.py
@@ -44,9 +44,14 @@ class GaussInt(object):
         if n == 0:
             return self.GI0
         elif n == 1:
-            return (1 / (2 * self.a)) * (np.exp(self.c) * (1 - np.exp(self.b - self.a)) + self.b * self.GI0)
+            # return (1 / (2 * self.a)) * (np.exp(self.c) * (1 - np.exp(self.b - self.a)) + self.b * self.GI0)
+            return (np.exp(c - a) / (4 * a**(3 / 2))) * (np.sqrt(np.pi) * b * np.exp(b**2 / (4 * a) + a) * \
+                   (erf((2 * a - b) / (2 * np.sqrt(a))) + erf(b / (2 * np.sqrt(a)))))
         elif n == 2:
-            return (1 / (4 * self.a ** 2)) * (np.exp(self.c) * (self.b - (2 * self.a + self.b) * np.exp(self.b - self.a)) + (2 * self.a + self.b ** 2) * self.GI0)
+            # return (1 / (4 * self.a ** 2)) * (np.exp(self.c) * (self.b - (2 * self.a + self.b) * np.exp(self.b - self.a)) + (2 * self.a + self.b ** 2) * self.GI0)
+            return (np.exp(c - a) / (8 * a**(5/2))) * (np.sqrt(np.pi) * (2 * a + b) * np.exp(b**2 / (4 * a) + a) * \
+                   (erf((2 * a - b) / (2 * np.sqrt(a))) + erf(b / (2 * np.sqrt(a)))) - 2 * np.sqrt(a) * (np.exp(b) * \
+                   (2 * a + b) - np.exp(a) * b))
         elif n == 3:
             return (1 / (8 * self.a ** 3)) * (np.exp(self.c) * (4 * self.a + self.b ** 2 - (4 * self.a ** 2 + 4 * self.a + 2 * self.a + self.b + self.b ** 2) * np.exp(self.b - self.a)) + self.b * (6 * self.a + self.b ** 2) * self.GI0)
         else:
@@ -104,10 +109,10 @@ def PolyGaussIntegrand1D(y, cx, cy, amp, x0, y0, sx, sy, rho):
 
         # Loop over the orders of the x IPV
         for n in range(N):
-            g += (amp[k] / norm) * cx[n] * GI(n)
+            g += cx[n] * GI(n)
 
     # We're done!
-    return f * g
+    return f * g * np.sum(amp) / (2 * norm ** len(amp))
 
 def Gauss2D(x, y, amp, x0, y0, sx, sy, rho):
     """A two-dimensional gaussian with arbitrary covariance."""
@@ -173,6 +178,7 @@ def PixelFlux(cx, cy, amp, x0, y0, sx, sy, rho, fast=True, **kwargs):
     else:
         F = lambda y, x: PolyGaussIntegrand2D(x, y, cx, cy, amp, x0, y0, sx, sy, rho)
         res, err = dblquad(F, 0, 1, lambda x: 0, lambda x: 1, **kwargs)
+
     return res
 
 def TestIntegration():
@@ -357,7 +363,7 @@ def _calculate_PSF_amplitude(mag):
     """
 
     # mag/flux relation constants
-    amp = 10**(-0.4*(mag-12))*1.74e5
+    amp = 10**(-0.4*(mag - 12))*1.74e5
     return amp
 
 if __name__ == '__main__':